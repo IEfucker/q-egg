c5c1bc496eeaf4a928f3f787198f9f8561ca46fb
IEFucker <baijinbo@126.com>
2018-08-22 14:21:01 +0800

    first commit

---
+++ database/migrations/20180821062829-m.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    const { STRING, INTEGER, DATE } = Sequelize
+    return queryInterface.createTable('user', {
+      _id: { type: INTEGER, primaryKey: true, autoIncrement: true }, // 用户id
+      loginname: { type: STRING(30) },
+      name: STRING(30),
+      password: STRING(32),
+      email: STRING(32),
+      url: STRING(100),
+      avatar: STRING(100),
+      githubId: STRING(30),
+      githubUsername: STRING(30),
+      githubAccessToken: STRING(100),
+      is_block: { type: INTEGER, defaultValue: 0 }, // 默认1是true
+      age: INTEGER,
+      active: { type: INTEGER, defaultValue: 0 }, // 默认1是true,
+      accessToken: { type: STRING },
+      created_at: { type: DATE },
+      updated_at: { type: DATE },
+      last_sign_in_at: DATE,
+    })
+  },
+
+  down: queryInterface => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.dropTable('user')
+  },
+};
---
+++ database/seeders/20180821065017-user.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: queryInterface => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('User', [{
+      _id: 1,
+      loginname: 'test',
+      name: 'test',
+      password: null,
+      email: '',
+      url: null,
+      avatar: '',
+      githubId: null,
+      githubUsername: null,
+      githubAccessToken: null,
+      is_block: 0, // 默认1是true
+      age: null,
+      active: 0, // 默认1是true,
+      accessToken: null,
+      created_at: '2018-8-20 00:40:40',
+      updated_at: '2018-8-21 02:58:27',
+      last_sign_in_at: null,
+    }], {});
+  },
+
+  down: queryInterface => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('User', null, {})
+  },
+};
